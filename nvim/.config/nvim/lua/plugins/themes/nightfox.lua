1aa79bb8b51cb96424583e75cbf5359756ace590
cnrmurphy <conor.murphy4304@gmail.com>
2025-10-28 14:36:10 -0400

    add nightfox theme

--- nvim/.config/nvim/lua/plugins/themes/nightfox.lua
+++ nvim/.config/nvim/lua/plugins/themes/nightfox.lua
@@ -3,13 +3,16 @@ return {
 	priority = 1000, -- Make sure to load this before all the other start plugins.
 	config = function()
 		require("nightfox").setup({
+			options = {
+				transparent = true,
+				terminal_colors = true,
+			},
 			styles = {
-				comments = { italic = false }, -- Disable italics in comments
 				keywords = "bold",
 			},
 		})
 
-		vim.o.background = "dark"
-		vim.cmd.colorscheme("nightfox")
+		-- vim.o.background = "dark"
+		vim.cmd.colorscheme("terafox")
 	end,
 }
